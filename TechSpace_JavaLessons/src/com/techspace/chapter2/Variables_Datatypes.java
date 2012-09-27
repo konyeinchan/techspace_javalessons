3f1ed24e13da75c6eac20d923e66088ccb5c256f
konyeinchan <konyeinchan@gmail.com>
2012-09-27 11:31:11 +0630

    FirstEgit

---
+++ TechSpace_JavaLessons/src/com/techspace/chapter2/Variables_Datatypes.java
@@ -0,0 +1,15 @@
+package com.techspace.chapter2;
+
+public class Variables_Datatypes {
+
+	/**
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a=1;
+		System.out.println("Byte"+a);
+	}
+
+}
